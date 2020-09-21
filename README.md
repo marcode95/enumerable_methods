28f4decb4f4d729aa2ea835e9624bfe3ee781fd8
Abhiyan001 <abhigyanmahanta001@gmail.com>
2020-09-21 18:49:47 +0530

    Fix Linter Checks

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-# enumerable_methods
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Enumerable-Methods
+
+This project is a microverse collaborative project which is part of the requirements of the microverse technical curriculum in the Ruby section.
+This project consist of different ruby enumerable methods that emulate Ruby's in-built enumerable methods
+
+<h3>Methods <a name = "method"></a></h3>
+<h4>The custom methods created are listed below:</h4>
+<li>my_each</li>
+<li>my_each_with_index</li>
+<li>my_select</li>
+<li>my_all?</li>
+<li>my_any</li>
+<li>my_none?</li>
+<li>my_count</li>
+<li>my_map</li>
+<li>my_inject</li>
+
+## Built With
+
+- Ruby
+- VS code
+
+## Getting Started
+
+**Run the code demonstrations below**
+
+```bash
+ruby enumerable.rb
+```
+
+### Prerequisites
+
+- Ruby
+
+## Setup
+
+To run this project locally, please ensure you have Ruby installed on your machine, then clone this repository by running.
+
+```bash
+git clone https://github.com/marcode95/enumerable_methods/tree/feature
+```
+
+After cloning run
+
+```bash
+cd enumerable-methods
+```
+
+Lastly run either of the commands listed in the getting started section above.
+
+## Authors
+
+👤 **Marco Erhardt**
+
+- Github: [@marcode95](https://github.com/marcode95)
+
+👤 **Abhigyan Mahanta**
+
+- Github: [@abhigyan001](https://github.com/abhigyan001)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration
+- etc
+
+## 📝 License
+
+License This project is a collaboration project of microverse students. All rights are reserved for Ntwali and Abhigyan.
--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,147 +1,133 @@
-
 module Enumerable
+  # my_each
 
-
-# my_each
-
-def my_each
-  arr = *self   
-  i = 0
+  def my_each
+    arr = *self
+    i = 0
     while i < arr.length
       yield(arr[i])
       i += 1
-    end  
-end
-
-
-# my_each_with_index
-
-def my_each_index
-  arr = *self   
-  i = 0
-  while i < arr.length
-    yield(arr[i], i)
-    i += 1
-  end   
-end
-
-
-#select
-
-def my_select
-  arr = *self  
-  new_arr = []
-  arr.my_each do |element|
-    next unless yield(element)
-    new_arr.push(element)
-  end
-  new_arr  
-end
-
-
-# my_all?
-
-def my_all?
-  return LocalJumpError unless block_given?
-  arr = *self  
-  condition = true
-  arr.my_each do |element|
-    if yield(element) == false
-      condition = false
-      break
     end
   end
-  condition  
-end
 
+  # my_each_with_index
 
-# my_any?
-
-def my_any?
-  arr = *self 
-  condition = false  
-  arr.my_each do |element|
-    if yield(element)
-      condition = true
-      break
+  def my_each_index
+    arr = *self
+    i = 0
+    while i < arr.length
+      yield(arr[i], i)
+      i += 1
     end
   end
-  condition
-end
 
+  # select
 
-# my_none?
-
-def my_none?  
- return LocalJumpError unless block_given?
-  arr = *self  
-  condition = true
-  arr.my_each do |element|
-    if yield(element) == true
-      condition = false
-      break
-    end
-  end
-  condition
-end
-
-
-#count
-
-def my_count
-  arr = *self
-  if block_given?
+  def my_select
+    arr = *self
     new_arr = []
     arr.my_each do |element|
       next unless yield(element)
+
       new_arr.push(element)
     end
-    puts new_arr.length
-  else
-    puts arr.length
+    new_arr
+  end
+
+  # my_all?
+
+  def my_all?
+    return LocalJumpError unless block_given?
+
+    arr = *self
+    condition = true
+    arr.my_each do |element|
+      if yield(element) == false
+        condition = false
+        break
+      end
+    end
+    condition
+  end
+
+  # my_any?
+
+  def my_any?
+    arr = *self
+    condition = false
+    arr.my_each do |element|
+      if yield(element)
+        condition = true
+        break
+      end
+    end
+    condition
+  end
+
+  # my_none?
+
+  def my_none?
+    return LocalJumpError unless block_given?
+
+    arr = *self
+    condition = true
+    arr.my_each do |element|
+      if yield(element) == true
+        condition = false
+        break
+      end
+    end
+    condition
+  end
+
+  # count
+
+  def my_count
+    arr = *self
+    if block_given?
+      new_arr = []
+      arr.my_each do |element|
+        next unless yield(element)
+
+        new_arr.push(element)
+      end
+      puts new_arr.length
+    else
+      puts arr.length
+    end
+  end
+
+  # my_map
+
+  def my_map(my_proc = nil)
+    return LocalJumpError unless block_given?
+
+    new_arr = []
+    if my_proc
+      my_each { |i| new_arr.push my_proc.call(i) }
+    else
+      my_each { |i| new_arr.push(yield(i)) }
+    end
+    new_arr
+  end
+
+  # my_inject
+
+  def my_inject(arg1)
+    arr = *self
+    return LocalJumpError unless block_given?
+
+    result = 0 + arg1
+    arr.my_each do |element|
+      result = yield(result, element)
+    end
+    result
+  end
+
+  # multiply_els
+
+  def multiply_els
+    arr = *self
+    arr.my_inject(1) { |sum, number| sum * number }
   end
 end
-
-
-#my_map
-
-def my_map(my_proc = nil)
-  return LocalJumpError unless block_given?  
-  new_arr = []
-  if (my_proc)
-    my_each  { |i| new_arr.push my_proc.call(i) }  
-  else
-    my_each { |i| new_arr.push (yield(i)) }
-  end  
-  new_arr
-end
-
-
-# my_inject
-
-def my_inject(arg1)
-  arr = *self
-  return LocalJumpError unless block_given?
-  result = 0 + arg1
-  arr.my_each do |element| 
-    result = yield(result, element)
-  end
-  result
-end
-
-
-# multiply_els
-
-def multiply_els
-  arr = *self
-  arr.my_inject(1) { |sum, number| sum * number }
-end
-
-end
-
-
-
-
-
-
-
