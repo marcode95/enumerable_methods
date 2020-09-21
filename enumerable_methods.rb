b3f96024ac20828fe83508fa74becbeb49ca6d46
Abhiyan001 <abhigyanmahanta001@gmail.com>
2020-09-21 13:43:16 +0530

    Modify my_each and my_each with_index

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,64 +1,38 @@
-# each
 
-def my_each(arr)
-  if arr.kind_of?(Array) == true 
-    i = 0
+# my_each
+module Enumerable
+def my_each
+  arr = *self   
+  i = 0
     while i < arr.length
       yield(arr[i])
       i += 1
     end  
-  else    
-    i = 0
-    while i < arr.length
-    yield(arr.keys[i], arr.values[i])
+end
+
+
+# arr4 = {"name" => "rick", "age" => 7, "hungry" => true}
+# arr4.my_each do |x, y| 
+#   puts "#{x}" " #{y}"
+# end
+
+# my_each_with_index
+
+def my_each_index
+  arr = *self   
+  i = 0
+  while i < arr.length
+    yield(arr[i], i)
     i += 1
-    end  
-  end  
+  end   
+end
 end
 
-=begin
-rescue => exception
-  
-end my_each(["name", "eric", 6, "hungry"]) do |x| 
-  puts "#{x}" 
-end
+# arr4 = {"name" => "rick", "age" => 7, "hungry" => true}
+# arr4.my_each_index do |element, index|
+#   puts element if index.even?
+# end
 
-my_each({"name" => "rick", "age" => 7, "hungry" => true}) do |x, y|
-  puts "#{x}" " #{y}" 
-end
-=end
-
-
-#each_with_index
-
-def my_each_index(arr)
-  if arr.kind_of?(Array) == true 
-    i = 0
-    while i < arr.length
-      yield(arr[i], i)
-      i += 1
-    end  
-  else    
-    i = 0
-    while i < arr.length
-    yield(arr.keys[i], arr.values[i], i)
-    i += 1
-    end  
-  end  
-end
-
-=begin
-my_arr = [1, 2, 3, 4, 5, 6]
-my_hash = {"name" => "rick", "age" => 7, "hungry" => true}
-
-my_each_index(my_arr) do |element, index|
-  puts element if index.even?
-end
-
-my_each_index(my_hash) do |key, value, index|
-  puts "#{key}" " #{value}" if index.even?
-end
-=end
 
 
 #select
@@ -129,93 +103,85 @@ hash = {"name" => "rick", "age" => 7, "hungry" => true}
 puts my_all?(my_arr){|x| x < 3}
 puts my_all?(hash){|k, v| v == ("rick" || true)}
 
+# # my_any?
 
-# my_any?
+# def my_any?(arr)
+#   condition = false  
+#   my_each(arr) do |element|
+#     if yield(element)
+#       condition = true
+#       break
+#     end
+#   end
+#   condition
+# end  
 
-def my_any?(arr)
-  condition = false  
-  my_each(arr) do |element|
-    if yield(element)
-      condition = true
-      break
-    end
-  end
-  condition
-end  
-
-#my_arr = [1, 2, 3, 4, 5, 6]
-#puts my_any?(my_arr){ |x| x >  6 }
-
-
-
-# my_none?
-
-def my_none?(arr)  
-  my_each(arr) do |element|
-    return true unless yield(element)
-    return false
-  end
-end    
-
-#my_arr = [1, 2, 3, 4, 5, 6]
-#puts my_none?(my_arr){ |x| x > 3 }
+# #my_arr = [1, 2, 3, 4, 5, 6]
+# #puts my_any?(my_arr){ |x| x >  6 }
 
 
 
-#count
+# # my_none?
 
-def my_count(arr)
-  if block_given?
-    new_arr = []
-    my_each(arr) do |element|
-      next unless yield(element)
-      new_arr.push(element)
-    end
-    puts new_arr.length
-  else
-    puts arr.length
-  end
-end
+# def my_none?(arr)  
+#   my_each(arr) do |element|
+#     return true unless yield(element)
+#     return false
+#   end
+# end    
 
-# my_arr = [1, 2, 3, 4, 5]
-# my_count(my_arr) 
+# #my_arr = [1, 2, 3, 4, 5, 6]
+# #puts my_none?(my_arr){ |x| x > 3 }
 
 
 
-#my_map
+# #count
 
-def my_map(arr)
-  new_arr = []
-  my_each(arr) do |element|    
-    new_arr.push(yield(element))
-  end
-  new_arr
-end
+# def my_count(arr)
+#   if block_given?
+#     new_arr = []
+#     my_each(arr) do |element|
+#       next unless yield(element)
+#       new_arr.push(element)
+#     end
+#     puts new_arr.length
+#   else
+#     puts arr.length
+#   end
+# end
 
-# my_arr = [1, 2, 3, 4, 5, 6]
-# puts my_map(my_arr){ |x| x + 3 }
-
-
-# my_inject
-
-def my_inject(arr, start)
-  result = 0 + start
-  my_each(arr) do |element| 
-    result = yield(result, element)
-  end
-  result
-end
-# my_arr = [1, 2, 3, 4, 5, 6]
-# puts my_inject(my_arr, 1){ |sum, number| sum * number }
+# # my_arr = [1, 2, 3, 4, 5]
+# # my_count(my_arr) 
 
 
 
-#multiply_els
+# #my_map
 
-def multiply_els(arr)
-  product = my_inject(arr, 1){ |sum, number| sum * number }
-  product
-end
-my_arr = [2, 4, 5]
-puts multiply_els(my_arr)
+# def my_map(arr)
+#   new_arr = []
+#   my_each(arr) do |element|    
+#     new_arr.push(yield(element))
+#   end
+#   new_arr
+# end
+
+# # my_arr = [1, 2, 3, 4, 5, 6]
+# # puts my_map(my_arr){ |x| x + 3 }
+
+
+# # my_inject
+
+# def my_inject(arr, start)
+#   result = 0 + start
+#   my_each(arr) do |element| 
+#     result = yield(result, element)
+#   end
+#   result
+# end
+# # my_arr = [1, 2, 3, 4, 5, 6]
+# # puts my_inject(my_arr, 1){ |sum, number| sum * number }
+
+
+# end
+
 
