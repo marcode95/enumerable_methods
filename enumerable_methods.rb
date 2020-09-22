6b3a48c7a1ace38c3e348a82a4d8f53ff17d9511
Abhiyan001 <abhigyanmahanta001@gmail.com>
2020-09-22 19:43:13 +0530

    Fix multiply_els method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -3,6 +3,7 @@ module Enumerable
 
   def my_each
     return enum_for(:my_each) unless block_given?
+
     arr = *self
     i = 0
     while i < arr.length
@@ -16,6 +17,7 @@ module Enumerable
 
   def my_each_with_index
     return enum_for(:my_each_with_index) unless block_given?
+
     arr = *self
     i = 0
     while i < arr.length
@@ -29,6 +31,7 @@ module Enumerable
 
   def my_select
     return enum_for(:my_select) unless block_given?
+
     arr = *self
     new_arr = []
     arr.my_each do |element|
@@ -54,17 +57,14 @@ module Enumerable
           condition = false
           break
         end
-      else
-        if block_given?        
-          if yield(element) == false || element == nil
+      elsif block_given?
+          if yield(element) == false || element.nil?
             condition = false
             break
           end
-        else        
-          if element == false || element == nil
+        elsif element == false || element.nil?
             condition = false
-            break
-          end
+            break          
         end
       end
     end
@@ -87,13 +87,13 @@ module Enumerable
           break
         end
       else
-        if block_given?        
-          if yield(element) || element == nil
+        if block_given?
+          if yield(element) || element.nil?
             condition = true
             break
           end
-        else        
-          if element || element == nil
+        else
+          if element || element.nil?
             condition = true
             break
           end
@@ -113,22 +113,21 @@ module Enumerable
 
   def my_count(arg = nil)
     arr = *self
-    if block_given? && arg == nil
+    if block_given? && arg.nil?
       new_arr = []
       arr.my_each do |element|
         next unless yield(element)
+
         new_arr.push(element)
       end
       new_arr.length
-    elsif (!block_given? && arg != nil) || (block_given? && arg != nil)
+    elsif (!block_given? && !arg.nil?) || (block_given? && !arg.nil?)
       new_arr = []
       arr.my_each do |element|
-        if element == arg
-          new_arr.push(element)
-        end
+        new_arr.push(element) if element == arg
       end
       new_arr.length
-    else 
+    else
       arr.length
     end
   end
@@ -136,8 +135,8 @@ module Enumerable
   # my_map
 
   def my_map(my_proc = nil)
-    return enum_for(:my_map
-    ) unless block_given?
+    return enum_for(:my_map) unless block_given?
+
     new_arr = []
     if my_proc
       my_each { |i| new_arr.push my_proc.call(i) }
@@ -151,6 +150,7 @@ module Enumerable
 
   def my_inject(arg1 = 0, arg2 = nil)
     return raise LocalJumpError if !block_given? && !arg1
+
     if block_given?
       result = arg1
       my_each do |element|
@@ -163,42 +163,17 @@ module Enumerable
       end
     elsif !block_given? && !arg2
       result = 0
-      if arg1 == (:* || "*")
-        result = 1
-      end
+      result = 1 if arg1 == (:* || '*')
       my_each do |element|
         result = result.send arg1, element
       end
     end
     result
-  end   
-end
-my_arr = [1, 2, 3, 4, 5, 6]
-puts my_arr.my_inject(:*)
-
-
-# multiply_els
-
-def multiply_els
-  arr = *self
-  arr.my_inject { |sum, number| sum * number }
+  end
 end
 
+def multiply_els(arg1 = nil)
+  arg1.my_inject(:*)
+end
 
-# #p [1, 2, 3].my_all? {|x| puts x}
-# p ["an", "bear", "ca"].my_all?(/t/)
-# p ["an", "bear", "ca"].my_any?(/t/)
-# p ["an", "bear", "ca"].my_none?(/t/)
-
-# #p {"name" => 1, "age" => 7, "hungry" => 4}.my_all? {|k, v| v > 3}
-
-
-# my_arr = [1,2,nil,4,5,56]
-# #puts my_arr.my_all?
-
-my_arr = [1, 2, 3, 2, 5]
- my_arr.my_count(2) { |x| x < 3 }
-
-
-
-
+# puts multiply_els([2, 4, 5])
